34f80789883f905c22f03ce74ac875b8265ab1a8
Alberto Leal <mail4alberto@gmail.com>
2026-04-15 18:44:24 -0400

    chore: add custom fork additions (git hash, version, docs)

--- zellij-utils/src/cli.rs
+++ zellij-utils/src/cli.rs
@@ -35,7 +35,7 @@ fn validate_session(name: &str) -> Result<String, String> {
 }
 
 #[derive(Parser, Default, Debug, Clone, Serialize, Deserialize)]
-#[clap(version, name = "zellij")]
+#[clap(version = concat!(env!("CARGO_PKG_VERSION"), " (", env!("GIT_HASH"), ")"), name = "zellij")]
 pub struct CliArgs {
     /// Maximum panes on screen, caution: opening more panes will close old ones
     #[clap(long, value_parser)]
